e5d3dd1ac6599f0ec62961136ca8e20ed847d4f1
MukhammadjonArabov <muhammadjonarabov04@gmail.com>
2025-11-23 22:14:54 +0500

    Add gunicorn

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,8 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv  
-
-load_dotenv('/home/hostdev/www/Portfolio-Website/.env')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
